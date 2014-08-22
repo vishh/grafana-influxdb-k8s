99748e0032fe26d3940d4dbec5285e39b8b7df29
Vishnu Kannan <vishnuk@google.com>
2014-08-22 20:12:39 +0000

    Adding elasticsearch containers for grafana.

--- grafana/set_elasticsearch.sh
+++ grafana/set_elasticsearch.sh
@@ -9,12 +9,16 @@ fi
 
 if [ -n "${ELASTICSEARCH_HOST}" ] && [ -n "${ELASTICSEARCH_PORT}" ]; then
     echo "=> Found Elasticsearch settings."
+    ip=${ELASTICSEARCH_HOST}
+    if [ "$ip" = "localhost"]; then
+	ip=$(curl "http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip" -H "X-Google-Metadata-Request: True")
+    fi
     if [ -n "${ELASTICSEARCH_USER}" ] && [ -n "${ELASTICSEARCH_PASS}" ]; then
-        echo "=> Set Elasticsearch url to \"${ELASTICSEARCH_PROTO}://${ELASTICSEARCH_USER}:${ELASTICSEARCH_PASS}@${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}\"."
-        sed -i "s#.*elasticsearch.*#elasticsearch:\"${ELASTICSEARCH_PROTO}://${ELASTICSEARCH_USER}:${ELASTICSEARCH_PASS}@${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}\",#g"  /app/config.js
+        echo "=> Set Elasticsearch url to \"${ELASTICSEARCH_PROTO}://${ELASTICSEARCH_USER}:${ELASTICSEARCH_PASS}@$ip:${ELASTICSEARCH_PORT}\"."
+        sed -i "s#.*elasticsearch.*#elasticsearch:\"${ELASTICSEARCH_PROTO}://${ELASTICSEARCH_USER}:${ELASTICSEARCH_PASS}@$ip:${ELASTICSEARCH_PORT}\",#g"  /app/config.js
     else
-        echo "=> Set Elasticsearch url to \"${ELASTICSEARCH_PROTO}://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}\"."
-        sed -i "s#.*elasticsearch.*#elasticsearch:\"${ELASTICSEARCH_PROTO}://${ELASTICSEARCH_HOST}:${ELASTICSEARCH_PORT}\",#g"  /app/config.js
+        echo "=> Set Elasticsearch url to \"${ELASTICSEARCH_PROTO}://$ip:${ELASTICSEARCH_PORT}\"."
+        sed -i "s#.*elasticsearch.*#elasticsearch:\"${ELASTICSEARCH_PROTO}://$ip:${ELASTICSEARCH_PORT}\",#g"  /app/config.js
     fi
     echo "=> Done!"
 else
